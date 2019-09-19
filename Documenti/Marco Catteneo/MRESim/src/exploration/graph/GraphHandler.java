c044a38b4f4f7ba3709db00ba595801cdedc27ea
Mralx <lonely94@live.it>
2019-09-19 20:11:47 +0200

    Creato singleton per la gestione del grafo

---
+++ Documenti/Marco Catteneo/MRESim/src/exploration/graph/GraphHandler.java
@@ -0,0 +1,33 @@
+package exploration.graph;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+public class GraphHandler {
+
+    private static GraphHandler handler;
+    private static ExplorationGraph graph;
+
+    public static GraphHandler getInstance(){
+        if (handler == null){
+            handler = new GraphHandler();
+            graph = new ExplorationGraph();
+        }
+        return handler;
+    }
+
+    public static ExplorationGraph getGraph(){
+        return graph;
+    }
+
+    public static void updateLocations(LinkedList<Point> locations){
+        graph.addNodesList(locations);
+    }
+
+    public static void updateFrontiers(LinkedList<Point> frontiers){
+        graph.addFrontiersList(frontiers);
+    }
+
+
+}
+
